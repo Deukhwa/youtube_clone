227df55ab49d2282c90bfb0a75844ccd91e92570
Deukhwa <deukhwajeong@gmail.com>
2022-01-10 09:52:07 -0500

    show video list with title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import './app.css'
+import { useEffect, useState } from 'react'
+import './App.css'
+import Videolist from './components/video_list/video_list'
 
 function App() {
-  return <h1>Hello :)</h1>
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    }
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyB5DvPBbluJSC8r0cYdLNulmJOZ4js47Gs',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error))
+  }, [])
+
+  return <Videolist videos={videos} />
 }
 
 export default App
